bb3e0f34aaa216f1e411bff742286df06721a8e4
margo-madeira <ligastaeva@mail.ru>
2024-05-15 15:30:01 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { Portfolio } from './Portfolio';
+import { Portfolio } from './components/Portfolio';
 
 function App() {
   return (
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,77 +4,6 @@ import { ProjectList } from "./ProjectList";
 
 export class Portfolio extends Component {
   state = {
-    //начал сост
-    projectsOriginal: [
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
-        category: "Business Cards",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
-        category: "Business Cards",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-        category: "Business Cards",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
-        category: "Flayers",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
-        category: "Business Cards",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-        category: "Business Cards",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
-        category: "Websites",
-      },
-      {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
-        category: "Flayers",
-      },
-    ],
     projects: [
       {
         img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
@@ -152,24 +81,23 @@ export class Portfolio extends Component {
   constructor() {
     super();
     this.onSelectFilter = this.onSelectFilter.bind(this);
+    this.projectsForFilter = this.state.projects;
   }
 
-  onSelectFilter(filter,arr) {
-    filter = filter.target.textContent;
-
-    if (filter !== "All") {
-      arr = this.state.projectsOriginal.filter((el) => {
-        if (el.category === filter) {
+  onSelectFilter(filter, arr) {
+    if (filter.target.textContent !== "All") {
+      arr = this.projectsForFilter.filter((el) => {
+        if (el.category === filter.target.textContent) {
           return el;
         }
       });
     } else {
-      arr = this.state.projectsOriginal;
+      arr = this.projectsForFilter;
     }
 
     this.setState({
       projects: arr,
-      selected: filter,
+      selected: filter.target.textContent,
     });
   }
 
